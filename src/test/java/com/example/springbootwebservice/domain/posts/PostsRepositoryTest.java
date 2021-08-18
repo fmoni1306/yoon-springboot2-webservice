2c51a1772d76950bffb739bdffb5d45cd55185e6
yoon <fmoni1306@gmail.com>
2021-08-18 18:03:07 +0900

    jpa

---
+++ src/test/java/com/example/springbootwebservice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.example.springbootwebservice.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void getPost() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author("fmoni@gmail.com").build());
+
+        // when
+        List<Posts> postList = postsRepository.findAll();
+
+        // then
+        Posts posts = postList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
--- src/test/java/com/example/springbootwebservice/web/HelloControllerTest.java
+++ src/test/java/com/example/springbootwebservice/web/HelloControllerTest.java
@@ -1,17 +1,11 @@
 package com.example.springbootwebservice.web;
 
-import com.example.springbootwebservice.web.HelloController;
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
 import static org.hamcrest.Matchers.is;
